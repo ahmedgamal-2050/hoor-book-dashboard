3bba5add1d6703eef5e18eb27a973fcfd4a4233b
ahmedgamal-2050 <eng.ahmedgamal2050@gmail.com>
2023-06-06 00:09:23 +0300

    set base api

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,4 @@
-//export const BASE_URL = 'http://localhost:8080';
 //export const BASE_API = 'http://143.110.170.3';
-export const BASE_URL = 'https://admin.hoorbookapp.com/';
 export const BASE_API = 'https://hoorbookapp.com/';
 
 export const lookups = [
